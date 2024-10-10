444122675fcfc84d3febe27e38f75d3af56d50bc
Nammiiro Zaharah bidin <zaharabidin485@gmail.com>
2024-10-10 18:24:51 +0300

    set up logging mechanism to capture errors and deployment events

--- client/src/index.js
+++ client/src/index.js
@@ -3,7 +3,16 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import {BrowserRouter} from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
+import * as Sentry from '@sentry/react';
+import { BrowserTracing } from '@sentry/tracing'; // Corrected import
+
+// Initialize Sentry
+Sentry.init({
+  dsn: "https://8d2f5507822baf48b3b6689465eba82c@o4508098312601600.ingest.de.sentry.io/4508098352382032",  
+  integrations: [new BrowserTracing()], // No changes needed here
+  tracesSampleRate: 1.0,  // Adjust this as necessary
+});
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -14,7 +23,4 @@ root.render(
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
